e96c99dfffc3feb1eda10905fd25dc8ab0efbd29
Artur Rymsza <artur.rymsza@gmail.com>
2017-11-27 22:51:01 +0100

    Add basic files

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+/*var a,
+	h,
+	triangleArea;
+	
+a = prompt('enter base');
+h = prompt('enter height');
+triangleArea = a*h/2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);*/
