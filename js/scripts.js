7c17ab212c318762e31b3bc99bea1b73f14e79b5
Artur Rymsza <artur.rymsza@gmail.com>
2017-11-28 18:48:15 +0100

    Change if-else statement content

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,10 @@ a = prompt('wpisz liczbę a');
 b = prompt('wpisz liczbę b');
 value = (a * a) - (2 * a * b) + (b * b);
 
-if (value <= 0) {
-	console.log('Wynik ujemny');
+if (value < 0) {
+	console.log('Wynik ujemny, ' + value);
+} else if (value > 0) {
+	console.log('Wynik dodatni, ' + value);
 } else {
-	console.log('Wynik dodatni');
+	console.log('Wynik to 0');
 }
